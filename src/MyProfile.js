37d849a39eed35c4f852962151014a0d3602c830
Drew <drewc273@gmail.com>
2019-01-18 17:29:19 -0500

    Started a grid for components on MyProfile page. Tweaked resizing for mobile a bit.

--- src/MyProfile.js
+++ src/MyProfile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import NaviBar from './Components/NavBar';
-import { Button, Thumbnail, Grid, Row, Col } from 'react-bootstrap';
+import { Button, Thumbnail, Grid, Row, Col, Image, Well } from 'react-bootstrap';
 
 class MyProfile extends Component {
   render() {
@@ -9,10 +9,20 @@ class MyProfile extends Component {
         <NaviBar/>
         <Grid>
           <Row>
-            <Col xs={6} md={2}>
+            <Col xs={6} sm={3} md={2}>
               <Thumbnail src={require('./avatarTest.png')}>
                   <Button bsStyle="primary" bsSize='xsmall'>Change Avatar</Button>
               </Thumbnail>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={3}>
+              <h3>Username:</h3>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={3}>
+              <h3>Major:</h3>
             </Col>
           </Row>
         </Grid>
