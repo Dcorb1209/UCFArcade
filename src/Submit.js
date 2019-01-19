35fa0a05b5866dd7a19bc186c76902586f49e426
Scott Kramarz <sscooter52@gmail.com>
2019-01-19 16:23:18 -0500

    More updates to submission page

--- src/Submit.js
+++ src/Submit.js
@@ -38,30 +38,34 @@ class Submit extends Component {
         <div className = 'SubmissionPage'>
         <h1>Submission</h1>
         <p>To submit your game you need to fill out the following page. Please include a one to three paragraph summary or desription of your game.</p>
-        
-        {/*Game Title Form*/}
-        <form id = 'gameTitle'>
-        <FormGroup
-          controlId="gameTitleForm"
-          validationState={this.getValidationState()}
-        >
-          <ControlLabel>Title of the game</ControlLabel>
-          <FormControl
+
+
+    <Form horizontal>           
+      {/*Game Title Form*/}
+      <FormGroup
+        controlId="gameTitleForm"
+        validationState={this.getValidationState()}
+      >
+        <Col componentClass={ControlLabel} sm={2}>Title of the game</Col>
+        <Col sm={5}>
+         <FormControl
             type="text"
             value={this.state.value}
-            placeholder="Enter the title of your game"
-            onChange={this.handleChange}
+              placeholder="Enter the title of your game"
+           onChange={this.handleChange}
           />
           <FormControl.Feedback />
+        </Col>
         </FormGroup>
-      </form>
-      <Form horizontal>
   <FormGroup controlId="formDescription">
     <Col componentClass={ControlLabel} sm={2}>
       Description
     </Col>
-    <Col sm={10}>
-      <FormControl type="text" placeholder="Description" />
+    <Col sm={5}>
+      <FormControl 
+        componentClass="textArea" 
+        type="text" 
+        placeholder="Description" />
     </Col>
   </FormGroup>
 
@@ -69,13 +73,53 @@ class Submit extends Component {
     <Col componentClass={ControlLabel} sm={2}>
       Controls
     </Col>
-    <Col sm={10}>
-      <FormControl type="text" placeholder="Controls" />
+    <Col sm={5}>
+      <FormControl 
+        componentClass="textArea" 
+        type="text" 
+        placeholder="Controls" />
     </Col>
   </FormGroup>
 
+  <FormGroup controlId="formVideo">
+    <Col componentClass={ControlLabel} sm={2}>
+      Video (Optional)
+    </Col>
+    <Col sm={5}>
+      <FormControl  
+        type="text" 
+        placeholder="Video (Youtube Link)" />
+    </Col>
+  </FormGroup>
+
+  <FormGroup controlId="formGameFile">
+    <Col componentClass={ControlLabel} sm={2}>
+      Game Files (Zip)
+    </Col>
+    <Col sm={5}>
+      <FormControl  
+        type="file" 
+        placeholder="Choose File" />
+    </Col>
+  </FormGroup>
+
+
+  <FormGroup controlId="formGameScreenShot">
+    <Col componentClass={ControlLabel} sm={2}>
+      Banner Screenshot
+    </Col>
+    <Col sm={5}>
+      <FormControl  
+        type="file" 
+        placeholder="Choose File" />
+    </Col>
+  </FormGroup>
+
+
+
+
   <FormGroup>
-    <Col smOffset={2} sm={10}>
+    <Col smOffset={2} sm={5}>
       <Checkbox>Genre 1</Checkbox>
       <Checkbox>Genre 2</Checkbox>
       <Checkbox>Genre 3</Checkbox>
