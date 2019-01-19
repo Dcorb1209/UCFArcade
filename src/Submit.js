dbc446c7a3439ade557f23d6e1ed9484b555bacd
Scott Kramarz <sscooter52@gmail.com>
2019-01-19 15:17:06 -0500

    Added a form to the submission page

--- src/Submit.js
+++ src/Submit.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react';
+import {FormGroup, FormControl, ControlLabel, HelpBlock} from 'react-bootstrap';
 import NaviBar from './Components/NavBar';
 import './Submit.css';
 
 class Submit extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      value: ''
+    };
+  }
+
+  getValidationState() {
+    const length = this.state.value.length;
+    if (length > 10) return 'success';
+    else if (length > 5) return 'warning';
+    else if (length > 0) return 'error';
+    return null;
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
   render() {
     return (
       <div className = 'FullPage'>
         <NaviBar/>
         <div className = 'SubmissionPage'>
-        <h1>Submit</h1>
+        <h1>Submission</h1>
+        <p>To submit your game you need to fill out the following page. Please include a one to three paragraph summary or desription of your game.</p>
+        <form>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+          <ControlLabel>Working example with validation</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.value}
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+          <HelpBlock>Validation is based on string length.</HelpBlock>
+        </FormGroup>
+      </form>
         </div>
       </div>
     )
