63b92ec9558fb5c5124a8b449566a95accc638b1
Scott Kramarz <sscooter52@gmail.com>
2019-01-19 15:49:42 -0500

    More updates to the submission page

--- src/Submit.js
+++ src/Submit.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {FormGroup, FormControl, ControlLabel, HelpBlock} from 'react-bootstrap';
+import {FormGroup, FormControl, ControlLabel, HelpBlock, Form, Button, Col, Checkbox} from 'react-bootstrap';
 import NaviBar from './Components/NavBar';
 import './Submit.css';
 
@@ -40,7 +40,7 @@ class Submit extends Component {
         <p>To submit your game you need to fill out the following page. Please include a one to three paragraph summary or desription of your game.</p>
         
         {/*Game Title Form*/}
-        <form>
+        <form id = 'gameTitle'>
         <FormGroup
           controlId="gameTitleForm"
           validationState={this.getValidationState()}
@@ -55,23 +55,42 @@ class Submit extends Component {
           <FormControl.Feedback />
         </FormGroup>
       </form>
+      <Form horizontal>
+  <FormGroup controlId="formDescription">
+    <Col componentClass={ControlLabel} sm={2}>
+      Description
+    </Col>
+    <Col sm={10}>
+      <FormControl type="text" placeholder="Description" />
+    </Col>
+  </FormGroup>
 
-      {/*Game Description Form*/}
-      <form>
-        <FormGroup
-          controlId="gameDescriptionForm"
-          validationState={this.getValidationStateDesc()}
-        >
-          <ControlLabel>Game Description</ControlLabel>
-          <FormControl
-            type="text"
-            value={this.state.value}
-            placeholder="Enter the description of your game"
-            onChange={this.handleChange}
-          />
-          <FormControl.Feedback />
-        </FormGroup>
-      </form>
+  <FormGroup controlId="formControls">
+    <Col componentClass={ControlLabel} sm={2}>
+      Controls
+    </Col>
+    <Col sm={10}>
+      <FormControl type="text" placeholder="Controls" />
+    </Col>
+  </FormGroup>
+
+  <FormGroup>
+    <Col smOffset={2} sm={10}>
+      <Checkbox>Genre 1</Checkbox>
+      <Checkbox>Genre 2</Checkbox>
+      <Checkbox>Genre 3</Checkbox>
+      <Checkbox>Genre 4</Checkbox>
+      <Checkbox>Genre 5</Checkbox>
+    </Col>
+  </FormGroup>
+
+  <FormGroup>
+    <Col smOffset={2} sm={10}>
+      <Button type="submit">Submit</Button>
+    </Col>
+  </FormGroup>
+</Form>;
+
         </div>
       </div>
     )
