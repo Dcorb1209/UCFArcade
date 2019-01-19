17e3966e8d42317513acbd6671cbd907ffe9a474
Armand Alvarez <aalvarez97@knights.ucf.edu>
2019-01-19 16:24:15 -0500

    Created template on About page. Added filler images and paragraphs.

--- src/About.js
+++ src/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Thumbnail, Grid, Row, Col } from 'react-bootstrap';
 import NaviBar from './Components/NavBar';
 
 class About extends Component {
@@ -7,6 +8,41 @@ class About extends Component {
       <div className = 'FullPage'>
         <NaviBar/>
         <h1>About</h1>
+
+        <Grid>
+          <Row>
+            <Col md={6} mdPush={6}>
+              <p>
+                Insert info here. Section 1 / 2
+              </p>
+              <p>
+                Culpa eiusmod laborum quis occaecat. Reprehenderit non dolor cillum elit eiusmod culpa ut qui cillum ea anim. Anim deserunt aliqua commodo nostrud sunt. Est exercitation deserunt nisi nulla mollit enim enim ut minim. Eiusmod aliquip nulla quis occaecat nulla voluptate dolor proident sunt ut mollit do eiusmod. Tempor excepteur ullamco quis nostrud commodo ex dolore occaecat officia et fugiat veniam laborum officia. Dolore Lorem nisi nostrud labore nisi veniam est ex Lorem non ex amet ullamco nisi.
+              </p>
+              <p>
+                Amet irure nisi dolor sint enim do nulla laborum aliquip incididunt. Magna reprehenderit laboris amet enim ea labore anim aute anim ullamco elit esse irure fugiat. Fugiat velit laborum veniam pariatur laborum cillum nostrud minim est ullamco nostrud non. Dolor et sint consequat occaecat nisi esse reprehenderit mollit sunt id. Labore exercitation officia deserunt minim est consequat labore laborum et velit sint consequat duis fugiat.
+              </p>
+            </Col>
+            <Col md={6} mdPull={6}>
+              <Thumbnail src={require('./Images/workers.jpg')}></Thumbnail>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={6} mdPush={6}>
+              <Thumbnail src={require('./Images/workers2.png')}></Thumbnail>
+            </Col>
+              <Col md={6} mdPull={6}>
+                <p>
+                  Insert info here. Section 2 / 2
+                </p>
+                <p>
+                  Culpa eiusmod laborum quis occaecat. Reprehenderit non dolor cillum elit eiusmod culpa ut qui cillum ea anim. Anim deserunt aliqua commodo nostrud sunt. Est exercitation deserunt nisi nulla mollit enim enim ut minim. Eiusmod aliquip nulla quis occaecat nulla voluptate dolor proident sunt ut mollit do eiusmod. Tempor excepteur ullamco quis nostrud commodo ex dolore occaecat officia et fugiat veniam laborum officia. Dolore Lorem nisi nostrud labore nisi veniam est ex Lorem non ex amet ullamco nisi.
+                </p>
+                <p>
+                  Amet irure nisi dolor sint enim do nulla laborum aliquip incididunt. Magna reprehenderit laboris amet enim ea labore anim aute anim ullamco elit esse irure fugiat. Fugiat velit laborum veniam pariatur laborum cillum nostrud minim est ullamco nostrud non. Dolor et sint consequat occaecat nisi esse reprehenderit mollit sunt id. Labore exercitation officia deserunt minim est consequat labore laborum et velit sint consequat duis fugiat.
+                </p>              </Col>
+          </Row>
+        </Grid>
       </div>
     )
   }
