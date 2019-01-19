46c1b0bab4c8ac634a90455e46ac7d38d4a85298
Drew <drewc273@gmail.com>
2019-01-19 00:26:39 -0500

    Added a slidesshow for games to the homepage

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import NaviBar from './Components/NavBar';
-import { Jumbotron, Button } from 'react-bootstrap';
+import { Jumbotron, Button, Carousel, Grid, Row, Col } from 'react-bootstrap';
 
 class Home extends Component {
   render() {
@@ -9,14 +9,43 @@ class Home extends Component {
       <div className="App">
         <NaviBar/>
         <Jumbotron>
-          <h1>Welcome to Arcade GRASS!</h1>
+          <h1>Knights Arcade</h1>
           <p>
-            Test
+            We need a new name
           </p>
           <p>
             <Button bsStyle="primary">Button</Button>
           </p>
         </Jumbotron>
+        <Grid>
+        <Row>
+        <Col md={8} mdOffset={2}>
+        <Carousel>
+          <Carousel.Item>
+            <img width={900} height={500} alt="900x500" src={require('./slideTest.png')} />
+            <Carousel.Caption>
+              <h3>Game 1</h3>
+              <p>Game 1 description bla bla bla</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width={900} height={500} alt="900x500" src={require('./slideTest.png')} />
+            <Carousel.Caption>
+              <h3>Game 2</h3>
+              <p>Game 2 description bla bla bla bla bla bla</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width={900} height={500} alt="900x500" src={require('./slideTest.png')} />
+            <Carousel.Caption>
+              <h3>Game 3</h3>
+              <p>Game 3 description bla bla bla... you get the point</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+        </Col>
+        </Row>
+        </Grid>
       </div>
     )
   }
