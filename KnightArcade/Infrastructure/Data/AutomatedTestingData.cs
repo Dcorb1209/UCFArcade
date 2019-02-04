7cb54e77d160d96474b1fdacf0d31059222d6459
Yitzak Hernandez <kre3ed@gmail.com>
2019-02-04 17:25:49 -0500

    combined backend with frontend

---
+++ KnightArcade/Infrastructure/Data/AutomatedTestingData.cs
@@ -0,0 +1,39 @@
+﻿using KnightArcade.Infrastructure.Data.Interface;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace KnightArcade.Infrastructure.Data
+{
+    public class AutomatedTestingData : IAutomatedTestingData
+    {
+        private readonly ILogger<AutomatedTestingData> _logger;
+
+        public AutomatedTestingData(ILogger<AutomatedTestingData> logger)
+        {
+            _logger = logger;
+        }
+
+
+        //TODO: Add config to game object file.
+        public bool PostGameForTesting(string url, object game, object config)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                string json = JsonConvert.SerializeObject(game);
+                client.PostAsJsonAsync(url, json);
+                return true;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message, e);
+                return false;
+            }
+        }
+    }
+}
---
+++ KnightArcade/Infrastructure/Data/Interface/IAutomatedTestingData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KnightArcade.Infrastructure.Data.Interface
+{
+    public interface IAutomatedTestingData
+    {
+        bool PostGameForTesting(string url, object game, object config);
+    }
+}
