d3b8c4c555bd89a74cdb4fa56e8c003908424f2d
JonNavas <40069322+JonNavas@users.noreply.github.com>
2019-02-13 20:27:36 -0500

    Added try/catch exceptions to testing functions

--- AutomatedTesting/AutomatedTesting/Infrastructure/Logic/TestingLogic.cs
+++ AutomatedTesting/AutomatedTesting/Infrastructure/Logic/TestingLogic.cs
@@ -16,6 +16,9 @@ namespace AutomatedTesting.Infrastructure.Logic
     {
         private readonly ILogger<TestingLogic> _logger;
         private readonly IS3Data _s3Data;
+        private readonly object _sync;
+        public TestingVariables testProcess = new TestingVariables();
+
 
         public TestingLogic(ILogger<TestingLogic> logger, IS3Data s3Data)
         {
@@ -25,70 +28,125 @@ namespace AutomatedTesting.Infrastructure.Logic
 
         public void Start()
         {
-            //string debugFilePath = null;
-            //string localFilePath = null;
-            string debugKey = "arcade_games/AlienDefense5(Final).zip";
-            string fileLocation = _s3Data.ReadObjectDataAsync(debugKey).Result;
+            try
+            {
+                lock(_sync)
+                {
+                    //string debugFilePath = null;
+                    //string localFilePath = null;
+                    string debugKey = "arcade_games/AlienDefense5(Final).zip";
+                    string fileLocation = _s3Data.ReadObjectDataAsync(debugKey).Result;
 
-            //get s3 and download zip file, return file location
+                    //get s3 and download zip file, return file location
 
-            //unzip file and returned unzip location
-            string unzipFile = Unzip(zipFile);
+                    //unzip file and returned unzip location
+                    string unzipFile = Unzip(fileLocation);
 
-            //Find .exe file path
-            string exeFile = findExe(unzipFile);
+                    //Find .exe file path
+                    string exeFile = FindExe(unzipFile);
 
-            //start .exe, check to see if it started
-            bool startTest = startFile(exeFile);
+                    //start .exe, check to see if it started
+                    bool startTest = StartFile(exeFile);
 
-            //Thread.Sleep(5000);
-            bool sleepTest = sleepFile(exeFile);
+                    //Thread.Sleep(5000);
+                    bool sleepTest = SleepFile(exeFile);
 
-            //stop .exe, check to see if it stopped
-            bool stopTest = stopFile(exeFile);
+                    //stop .exe, check to see if it stopped
+                    bool stopTest = StopFile(exeFile);
+                }
+            }
+            catch(Exception e)
+            {
+                _logger.LogError(e.Message, e);
+            }
+            
 
         }
 
         public string Unzip(string zipFile)
         {
-            string extractPath = @".\extract";
+            try
+            {
+                string extractPath = @".\extract";
 
-            ZipFile.ExtractToDirectory(zipFile, extractPath);
+                ZipFile.ExtractToDirectory(zipFile, extractPath);
 
-            return extractPath;
+                return extractPath;
+            }
+            catch(Exception e)
+            {
+                _logger.LogError(e.Message, e);
+                return null;
+            }
         }
 
-        string findExe(string unzipFile)
+        string FindExe(string unzipFile)
         {
-            string[] files = System.IO.Directory.GetFiles(unzipFile, "*.exe");
+            try
+            {
+                string[] files = System.IO.Directory.GetFiles(unzipFile, "*.exe");
 
-            return files[0];
-        }
+                return files[0];
+            }
 
-        public bool startFile(string exeFile)
-        {
-            Process.Start(exeFile);
-
-            return Process.GetProcessesByName(exeFile)
-                .FirstOrDefault(p => p.MainModule.FileName.StartsWith(@"c:\loc1")) != default(Process);
-        }
-
-        public bool sleepFile(string exeFile)
-        {
-            Thread.Sleep(5000);
-
-            return Process.GetProcessesByName(exeFile)
-               .FirstOrDefault(p => p.MainModule.FileName.StartsWith(@"c:\loc1")) != default(Process);
-        }
-
-        public bool stopFile(string exeFile)
-        {
+            catch(Exception e)
+            {
+                _logger.LogError(e.Message, e);
+                return null;
+            }
             
+        }
 
-            Thread.Sleep(300);
+        public bool StartFile(string exeFile)
+        {
+            try
+            {
+               testProcess.GameProcess = Process.Start(exeFile);
 
-            return Process.GetProcessesByName(exeFile)
-               .FirstOrDefault(p => p.MainModule.FileName.StartsWith(@"c:\loc1")) != default(Process);
+                return Process.GetProcessesByName(exeFile)
+                    .FirstOrDefault(p => p.MainModule.FileName.StartsWith(@"c:\loc1")) != default(Process);
+            }
+
+            catch(Exception e)
+            {
+                _logger.LogError(e.Message, e);
+                return false;
+            }         
+        }
+
+        public bool SleepFile(string exeFile)
+        {
+            try
+            {
+                Thread.Sleep(5000);
+
+                return Process.GetProcessesByName(exeFile)
+                   .FirstOrDefault(p => p.MainModule.FileName.StartsWith(@"c:\loc1")) != default(Process);
+            }
+
+            catch(Exception e)
+            {
+                _logger.LogError(e.Message, e);
+                return false;
+            }
+        }
+
+        public bool StopFile(string exeFile)
+        {
+            try
+            {
+                testProcess.GameProcess.Close();
+                Thread.Sleep(300);
+
+                return Process.GetProcessesByName(exeFile)
+                   .FirstOrDefault(p => p.MainModule.FileName.StartsWith(@"c:\loc1")) != default(Process);
+            }
+
+            catch(Exception e)
+            {
+                _logger.LogError(e.Message, e);
+                return false;
+            }
         }
     }
 }
